644c5ddecd915d321713cace8edebefcba00db07
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2023-12-03 11:27:18 +0100

    extraction of samples based on expression of target gene

--- collection/extract_subset.py
+++ collection/extract_subset.py
@@ -1,14 +1,20 @@
 ### Boas Pucker ###
-### bpucker@cebitec.uni-bielefeld.de ###
-### v0.25 ###
+### b.pucker@tu-bs.de ###
+### v0.3 ###
 
 __usage__ = """
 					python3 extract_subset.py
 					--in <FULL_PATH_TO_EXPRESSION_INPUT_FILE>
 					--out <FULL_PATH_TO_EXPRESSION_OUTPUT_FILE>
+					
+					OPTION1:
 					--accessions <FULL_PATH_TO_FILE_WITH_ACCESSION_NUMBERS>
 					
-					bug reports and feature requests: bpucker@cebitec.uni-bielefeld.de
+					OPTION2:
+					--gene <GENE_ID>
+					--cutoff <MINIMAL_GENE_EXPRESSION>
+					
+					bug reports and feature requests: b.pucker@tu-bs.de
 					"""
 
 import sys, os
@@ -90,29 +96,52 @@ def generate_output_file( accessions, exp_data, output_file ):
 			out.write( "\t".join( map( str, new_line ) )+"\n" )
 
 
+def get_valid_accessions( gene_id, cutoff, exp_data ):
+	"""! @brief identify accesssions with high expression of target gene """
+	
+	valid_accessions = []
+	all_accessions = list( exp_data.keys() )
+	for accession in all_accessions:
+		try:
+			if exp_data[ accession ][ gene_id ] >= cutoff:
+				valid_accessions.append( accession )
+		except KeyError:
+			pass
+	return valid_accessions
+
+
 def main( arguments ):
 	"""! @brief run everything """
 	
 	input_file = arguments[ arguments.index('--in')+1 ]
 	output_file = arguments[ arguments.index('--out')+1 ]
-	sample_file = arguments[ arguments.index('--accessions')+1 ]
+	
 	
 	if output_file[0] == "/":	#only generate new folder if absolute path is given
 		output_dir = "/".join( output_file.split('/')[:-1] )
 		if not os.path.exists( output_dir ):
 			os.makedirs( output_dir )
 	
-	accessions = load_accessions( sample_file )
-	
-	sys.stdout.write( "number of detected accessions: " + str( len( accessions ) ) + '\n' )
-	sys.stdout.flush()
-	
-	exp_data = load_expression_values( input_file )
+	if '--accessions' in arguments:
+		sample_file = arguments[ arguments.index('--accessions')+1 ]
+		accessions = load_accessions( sample_file )
+		sys.stdout.write( "number of detected accessions: " + str( len( accessions ) ) + '\n' )
+		sys.stdout.flush()
+		exp_data = load_expression_values( input_file )
+	elif '--gene' in arguments and '--cutoff' in arguments:
+		gene_id = arguments[ arguments.index('--gene')+1 ]
+		cutoff = float( arguments[ arguments.index('--cutoff')+1 ] )
+		exp_data = load_expression_values( input_file )
+		accessions = get_valid_accessions( gene_id, cutoff, exp_data )
+		sys.stdout.write( "number of valid accessions: " + str( len( accessions ) ) + '\n' )
+		sys.stdout.flush()
 	
 	generate_output_file( accessions, exp_data, output_file )
 
 
 if '--in' in sys.argv and '--out' in sys.argv and '--accessions' in sys.argv:
+	main( sys.argv )
+elif '--in' in sys.argv and '--out' in sys.argv and '--gene' in sys.argv and '--cutoff' in sys.argv:
 	main( sys.argv )
 else:
 	sys.exit( __usage__ )
