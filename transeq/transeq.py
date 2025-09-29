d0ac9cc29dbf3ce32dbc19f446a4bc3b6124f3c6
Nancy Choudhary <nancychoudhary139@gmail.com>
2025-09-29 14:33:58 +0200

    Updated to transeq.py v0.3
    Translates unknown/ambiguous codons (e.g., containing N) to 'X'. If --internal-stop-to-x is provided, internal '*' are converted to 'X' (a final '*' is kept).

--- transeq/transeq.py
+++ transeq/transeq.py
@@ -1,112 +1,134 @@
 ### Boas Pucker ###
 ### bpucker@cebitec.uni-bielefelde ###
-### v0.2 ###
+#!/usr/bin/env python3
+# v0.3
 
 __usage__ = """
-					python transeq.py
-					--in <FULL_PATH_TO_INPUT_FILE|INPUT_FOLDER>
-					--out <FULL_PATH_TO_OUTPUT_FILE|OUTPUT_FOLDER>
-					
-					bug reports and feature requests: bpucker@cebitec.uni-bielefeld.de
-					"""
+python transeq.py --in <INPUT_FILE|INPUT_DIR> --out <OUTPUT_FILE|OUTPUT_DIR> [--internal-stop-to-x]
 
+Notes:
+- Unknown/ambiguous codons (e.g., containing N) translate to 'X'.
+- If --internal-stop-to-x is provided, internal '*' are converted to 'X' (a final '*' is kept).
+"""
 
-import sys, glob, os
+import sys, os, glob
 
-# --- end of imports --- #
+# --- helpers --- #
 
+def load_multiple_fasta_file(fasta_file):
+    """Load all sequences from a (possibly wrapped) FASTA file into a dict."""
+    content = {}
+    header = None
+    seq_chunks = []
+    with open(fasta_file, "r") as f:
+        for line in f:
+            if not line.strip():
+                continue
+            if line.startswith(">"):
+                if header is not None:
+                    content[header] = "".join(seq_chunks)
+                header = line[1:].strip().split()[0]  # trim after first whitespace
+                seq_chunks = []
+            else:
+                seq_chunks.append(line.strip())
+        if header is not None:
+            content[header] = "".join(seq_chunks)
+    return content
 
-def load_genetic_code( genetic_code_file ):
-	"""! @brief load genetic code from file """
-	
-	genetic_code = {}
-	
-	with open( genetic_code_file, "r" ) as f:
-		line = f.readline()
-		while line:
-			parts = line.strip().split('\t')
-			codons = parts[1].split(', ')
-			for codon in codons:
-				genetic_code.update( { codon: parts[0] } )
-			line = f.readline()
-	return genetic_code
+def translate(seq, genetic_code, unknown="X", internal_stop_to_x=False, keep_terminal_stop=True):
+    """Translate DNA to protein.
+       - Unknown/ambiguous codons -> 'X'
+       - Stop codons per table -> '*'
+       - Optionally convert internal '*' to 'X' (keeps final '*' if present).
+    """
+    seq = seq.upper().replace("U", "T")
+    pep = []
+    n = len(seq) // 3
+    for i in range(n):
+        codon = seq[i*3:i*3+3]
+        aa = genetic_code.get(codon)
+        if aa is None:
+            aa = unknown
+        pep.append(aa)
+    pep = "".join(pep)
 
+    if internal_stop_to_x and "*" in pep:
+        if keep_terminal_stop and pep.endswith("*"):
+            pep = pep[:-1].replace("*", "X") + "*"
+        else:
+            pep = pep.replace("*", "X")
+    return pep
 
-def load_multiple_fasta_file( fasta_file ):
-	"""! @brief load all sequences from multiple fasta file """
-	
-	content = {}
-	
-	with open( fasta_file, "r" ) as f:
-		header = f.readline().strip()[1:]
-		if " " in header:
-			header = header.split(' ')[0]
-		line = f.readline()
-		seq = ""
-		while line:
-			if line[0] == '>':
-				content.update( { header: seq } )
-				header = line.strip()[1:]
-				if " " in header:
-					header = header.split(' ')[0]
-				seq = ""
-			else:
-				seq += line.strip()
-			line = f.readline()
-		content.update( { header: seq } )
-	return content
+def translate_file(in_fa, out_fa, genetic_code, internal_stop_to_x=False):
+    seqs = load_multiple_fasta_file(in_fa)
+    internal_stop_count = 0
+    with open(out_fa, "w") as out:
+        for header, nt in seqs.items():
+            pep = translate(nt, genetic_code, unknown="X",
+                            internal_stop_to_x=internal_stop_to_x,
+                            keep_terminal_stop=True)
+            if "*" in pep[:-1]:
+                internal_stop_count += 1
+            out.write(f">{header}\n{pep}\n")
+    print(f"[INFO] {os.path.basename(in_fa)} -> {os.path.basename(out_fa)} | "
+          f"seqs: {len(seqs)} | internal-stops (pre-fix): {internal_stop_count}")
 
+def gather_inputs(spec):
+    """Return list of input files. If directory, grab common FASTA extensions."""
+    if os.path.isdir(spec):
+        exts = ("*.fa", "*.fasta", "*.fna", "*.fas")
+        files = []
+        for ext in exts:
+            files.extend(glob.glob(os.path.join(spec, ext)))
+        files.sort()
+        return files
+    else:
+        return [spec]
 
-def get_longest_peptide_per_seq( genetic_code, sequences, output_file ):
-	"""! @brief get longest encoded peptide per seq and write it into output file """
-	
-	with open( output_file, "w" ) as out:
-		for key in sequences.keys():
-			seq = sequences[ key ]
-			peptide = translate( seq, genetic_code )
-			out.write( '>' + key + '\n' + peptide + '\n' )
+def make_output_path(out_spec, in_file):
+    """Decide output path: if out_spec is a dir or endswith '/', write there; else treat as file path."""
+    if out_spec.endswith(os.sep) or os.path.isdir(out_spec):
+        os.makedirs(out_spec, exist_ok=True)
+        base = os.path.basename(in_file)
+        root = os.path.splitext(base)[0]
+        return os.path.join(out_spec, f"{root}.pep.fa")
+    else:
+        # Single input -> exact output file path
+        # Multi-input with a single-file out_spec is not supported
+        return out_spec
 
+def main(argv):
+    if '--in' not in argv or '--out' not in argv:
+        sys.exit(__usage__)
 
-def translate( seq, genetic_code ):
-	"""! @brief translates the given nucleotide sequence into peptide and splits at each star (stop codon) """
-	
-	seq = seq.upper()
-	
-	peptide = []
-	for i in range( int( len( seq ) / 3.0 ) ):
-		codon = seq[i*3:i*3+3]
-		try:
-			peptide.append( genetic_code[ codon ] )
-		except:
-			peptide.append( "*" )
-	return "".join( peptide )
+    input_spec = argv[argv.index('--in')+1]
+    output_spec = argv[argv.index('--out')+1]
+    internal_stop_to_x = ('--internal-stop-to-x' in argv)
 
+    # Standard code table (nuclear, 1)
+    genetic_code = {
+        'CTT':'L','ATG':'M','AAG':'K','AAA':'K','ATC':'I','AAC':'N','ATA':'I','AGG':'R',
+        'CCT':'P','ACT':'T','AGC':'S','ACA':'T','AGA':'R','CAT':'H','AAT':'N','ATT':'I',
+        'CTG':'L','CTA':'L','CTC':'L','CAC':'H','ACG':'T','CCG':'P','AGT':'S','CAG':'Q',
+        'CAA':'Q','CCC':'P','TAG':'*','TAT':'Y','GGT':'G','TGT':'C','CGA':'R','CCA':'P',
+        'TCT':'S','GAT':'D','CGG':'R','TTT':'F','TGC':'C','GGG':'G','TGA':'*','GGA':'G',
+        'TGG':'W','GGC':'G','TAC':'Y','GAG':'E','TCG':'S','TTA':'L','GAC':'D','TCC':'S',
+        'GAA':'E','TCA':'S','GCA':'A','GTA':'V','GCC':'A','GTC':'V','GCG':'A','GTG':'V',
+        'TTC':'F','GTT':'V','GCT':'A','ACC':'T','TTG':'L','CGT':'R','TAA':'*','CGC':'R'
+    }
 
-def main( arguments ):
-	
-	input_spec = arguments[ arguments.index( '--in' )+1 ]
-	output_spec = arguments[ arguments.index( '--out' )+1 ]
-	
-	genetic_code = {'CTT': 'L', 'ATG': 'M', 'AAG': 'K', 'AAA': 'K', 'ATC': 'I', 'AAC': 'N', 'ATA': 'I', 'AGG': 'R', 'CCT': 'P', 'ACT': 'T', 'AGC': 'S', 'ACA': 'T', 'AGA': 'R', 'CAT': 'H', 'AAT': 'N', 'ATT': 'I', 'CTG': 'L', 'CTA': 'L', 'CTC': 'L', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'AGT': 'S', 'CAG': 'Q', 'CAA': 'Q', 'CCC': 'P', 'TAG': '*', 'TAT': 'Y', 'GGT': 'G', 'TGT': 'C', 'CGA': 'R', 'CCA': 'P', 'TCT': 'S', 'GAT': 'D', 'CGG': 'R', 'TTT': 'F', 'TGC': 'C', 'GGG': 'G', 'TGA': '*', 'GGA': 'G', 'TGG': 'W', 'GGC': 'G', 'TAC': 'Y', 'GAG': 'E', 'TCG': 'S', 'TTA': 'L', 'GAC': 'D', 'TCC': 'S', 'GAA': 'E', 'TCA': 'S', 'GCA': 'A', 'GTA': 'V', 'GCC': 'A', 'GTC': 'V', 'GCG': 'A', 'GTG': 'V', 'TTC': 'F', 'GTT': 'V', 'GCT': 'A', 'ACC': 'T', 'TTG': 'L', 'CGT': 'R', 'TAA': '*', 'CGC': 'R'}
-	
-	if input_spec[-1] == "/":
-		input_files = glob.glob( input_file + "*.fasta" ) + glob.glob( input_file + "*.fa" )
-	else:
-		input_files = [ input_spec ]
-	for filename in input_files:
-		if output_spec[-1] == "/":
-			output_file = output_spec + filename.split('/')[-1].split('.')[0] + ".pep.fa"
-		else:
-			output_file = output_spec + ""
-		sequences = load_multiple_fasta_file( filename )
-		print "number of seqs detected for translation: " + str( len( sequences.keys() ) )
-		get_longest_peptide_per_seq( genetic_code, sequences, output_file )
+    inputs = gather_inputs(input_spec)
 
+    # If multiple inputs but output_spec is a file path, abort to avoid overwriting
+    if len(inputs) > 1 and (not output_spec.endswith(os.sep) and not os.path.isdir(output_spec)):
+        sys.exit("[ERROR] For multiple input files, --out must be a directory or end with '/'.")
+    if output_spec.endswith(os.sep):
+        os.makedirs(output_spec, exist_ok=True)
+
+    for in_file in inputs:
+        out_file = make_output_path(output_spec, in_file)
+        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+        translate_file(in_file, out_file, genetic_code, internal_stop_to_x=internal_stop_to_x)
 
 if __name__ == '__main__':
-	
-	if '--in' in sys.argv and '--out' in sys.argv:
-		main( sys.argv )
-	else:
-		sys.exit( __usage__ )
-	print "all done!"
+    main(sys.argv)
