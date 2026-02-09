892aa89528162ffab3207d5e5ce16de868f9085a
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2026-02-09 11:52:33 +0100

    v0.3 header issue fixed

--- heatmapper/heatmap_plotter.py
+++ heatmapper/heatmap_plotter.py
@@ -1,15 +1,16 @@
 ### Boas Pucker ###
-### b.pucker@tu-bs.de ###
-### v0.1 ###
+### pucker@uni-bonn.de ###
+### v0.3 ###
 
 __usage__ = """
-					python heatmap_plotter.py
+					python3 heatmap_plotter.py
 					--genes <GENES_FILE>
 					--exp <EXPRESSION_FILE>
 					--out <FIGURE_OUTPUT_FILE>
 					
 					optional:
 					--norm (activates normalization per gene)
+					--samples <SAMPLE_METADATA>
 					"""
 
 import os, sys
@@ -47,7 +48,9 @@ def load_exp( gene_file ):
 	
 	exp = {}
 	with open( gene_file, "r" ) as f:
-		sample_order = f.readline().strip().split('\t')[1:]
+		sample_order = f.readline().strip().split('\t')
+		if sample_order[0] in [ "genes", "gene" ]:
+			sample_order = sample_order[1:]
 		for header in sample_order:
 			exp.update( { header: {} } )
 		line = f.readline()
@@ -60,7 +63,7 @@ def load_exp( gene_file ):
 	return exp, sample_order
 
 
-def generate_gene_exp_figure( figfile, genes, exp, gene_order, sample_order, norm ):
+def generate_gene_exp_figure( figfile, genes, exp, gene_order, sample_order, norm, samples ):
 	"""! @brief generate figure """
 	
 	exp_dataset = []	#[ [ column1 ], [column2], [column3] ]
@@ -71,7 +74,13 @@ def generate_gene_exp_figure( figfile, genes, exp, gene_order, sample_order, nor
 			values.append( exp[ sample ][ gene ] )
 		exp_dataset.append( values )
 		gene_lables.append( genes[ gene ] )
-	data = pd.DataFrame( exp_dataset, columns=sample_order )
+	sample_name_display = []
+	for each in sample_order:
+		try:
+			sample_name_display.append( samples[ each ] )
+		except KeyError:
+			sample_name_display.append( each )
+	data = pd.DataFrame( exp_dataset, columns=sample_name_display )
 	data.index = gene_lables + []
 	
 	if norm:
@@ -79,11 +88,12 @@ def generate_gene_exp_figure( figfile, genes, exp, gene_order, sample_order, nor
 	
 	fig, ax = plt.subplots()
 	
-	h = sns.heatmap( data, annot=True, fmt=".1f", cmap="crest" )
+	h = sns.heatmap( data, annot=True, fmt=".1f", cmap="crest", annot_kws={"size": 3}, xticklabels = 1, yticklabels=1 )
 	
 	ax.set(xlabel="", ylabel="")
 	ax.xaxis.tick_top()
-	h.set_yticklabels( h.get_yticklabels(), rotation=0)	#turn y-axis labels to horizontal
+	h.set_yticklabels( h.get_yticklabels(), rotation=0, size = 6 )	#turn y-axis labels to horizontal
+	h.set_xticklabels( h.get_xticklabels(), rotation=90, size = 6 )	#turn y-axis labels to horizontal
 	
 	fig.tight_layout()
 	
@@ -91,6 +101,22 @@ def generate_gene_exp_figure( figfile, genes, exp, gene_order, sample_order, nor
 	
 	fig.savefig( figfile, dpi=300 )
 	plt.close( "all" )
+
+
+def load_sample_names( sample_file ):
+	"""! @brief load sample names from input file """
+	
+	samples = {}
+	sample_order = []
+	with open( sample_file, "r" ) as f:
+		line = ''.join([ i if ord(i) < 128 else ' ' for i in f.readline() ])
+		while line:
+			parts = line.strip().split('\t')
+			if len( parts ) > 1:
+				samples.update( { parts[0]: parts[1] } )
+				sample_order.append( parts[0] )
+			line = ''.join([ i if ord(i) < 128 else ' ' for i in f.readline() ])
+	return samples, sample_order
 
 
 def main( arguments ):
@@ -108,8 +134,14 @@ def main( arguments ):
 	genes, gene_order = load_genes( gene_file )
 
 	exp, sample_order = load_exp( exp_file )
+	
+	if '--samples' in arguments:
+		sample_file = arguments[ arguments.index('--samples')+1 ]
+		samples, sample_order = load_sample_names( sample_file )
+	else:
+		samples = {}
 
-	generate_gene_exp_figure( figfile, genes, exp, gene_order, sample_order, norm )
+	generate_gene_exp_figure( figfile, genes, exp, gene_order, sample_order, norm, samples )
 
 
 if '--genes' in sys.argv and '--exp' in sys.argv and '--out' in sys.argv:
